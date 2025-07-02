d56ade10c846adf3c5ee3027f4beab1edbaa893e
shivraj-chavan <shivrajchavan1902@gmail.com>
2025-07-02 20:08:20 +0000

    image url updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['69.62.84.113'], 
+        domains: ['69.62.84.113','api-dev.majhinavimumbai.com'], 
       }
 }
 
