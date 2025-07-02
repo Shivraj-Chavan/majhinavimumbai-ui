17560dfe3826a7c03517c47409d41683726f031f
shivraj-chavan <shivrajchavan1902@gmail.com>
2025-07-02 20:00:40 +0000

    url updated

--- src/constance.js
+++ src/constance.js
@@ -1,6 +1,6 @@
  const CONFIG={
     API_BASE_URL:"https://api-dev.majhinavimumbai.com/api",
-    IMAGE_BASE_URL:"https://api-dev.majhinavimumbai.com/",
+    IMAGE_BASE_URL:"https://api-dev.majhinavimumbai.com",
     // API_BASE_URL:"http://185.230.64.102:5000/api",
     // IMAGE_BASE_URL:"http://185.230.64.102:5000",
     // API_BASE_URL:"http://69.62.84.113:5005/api",
